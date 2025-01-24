2f96dea4beee736cac9491b636b8fdadd12be930
yash jain <161117850+yashjain15357@users.noreply.github.com>
2025-01-24 19:45:45 +0530

    background change

---
+++ bg_col_cng04/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive"); // Default color
+
+  return (
+    <div className="h-screen w-screen"
+      style={{ backgroundColor: color }}>
+      <div className="bg-amber-400 text-center rounded-full" >
+        <button
+          className="rounded-full m-3"
+          onClick={() => setColor("red")}
+        >RED</button>
+
+        <button
+          className="rounded-full m-3"
+          onClick={() => setColor("blue")
+          }>BLUE</button>
+
+        <button
+          className="rounded-full m-3"
+          onClick={() => setColor("green"
+          )}>GREEN</button>
+
+        <button
+          className="rounded-full m-3"
+          onClick={() => setColor("yellow"
+          )}>YELLOW</button>
+        <button
+          className="rounded-full m-3"
+          onClick={() => setColor("greenyellow"
+          )}>GREENYELLOW</button>
+        <button
+          className="rounded-full m-3"
+          onClick={() => setColor("skyblue"
+          )}>SKYBLUE</button>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
